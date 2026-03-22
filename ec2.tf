46fecbf9c0288fd0c9f214750e99821ad5ea4208
Mariusz Brozda <mariuszbrozda.dublin@gmail.com>
2026-03-22 23:44:51 +0000

    Add EC2 provisioners to install Apache webserver

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,35 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids      = [aws_security_group.sg.id]
   key_name                    = aws_key_pair.aws_keypair.key_name
 
+  provisioner "local-exec" {
+    command    = "echo ${var.instance_name}:${self.private_ip} >> ec2s.txt"
+    on_failure = continue
+  }
+
+  provisioner "file" {
+    source      = "install_httpd.sh"
+    destination = "/tmp/install_httpd.sh"
+    on_failure  = continue
+  }
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.ec2_instance.public_ip
+    user        = "ec2-user"
+    private_key = file("~/.ssh/aws-golden-key")
+    timeout     = "5m"
+  }
+
+  provisioner "remote-exec" {
+    inline     = ["chmod u+x /tmp/install_httpd.sh", "/tmp/install_httpd.sh"]
+    on_failure = continue
+  }
+
+  provisioner "remote-exec" {
+    inline     = ["sudo hostnamectl set-hostname ${var.instance_name}"]
+    on_failure = continue
+  }
+
   tags = {
     Name = "${var.prefix}-${var.instance_name}"
   }
--- outputs.tf
+++ outputs.tf
@@ -20,3 +20,7 @@ output "ec2_public_ip_address" {
   description = "EC2 Public IP"
   value       = aws_instance.ec2_instance.public_ip
 }
+
+output "webserver_url" {
+  value = join("//", ["http:", aws_instance.ec2_instance.public_ip])
+}
