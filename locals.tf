56e908b940b675eb828b7e6ed74ee6a800b6962d
Mariusz Brozda <mariuszbrozda.dublin@gmail.com>
2026-04-15 20:39:46 +0100

    Add internet gateway and subnet resources to VPC module

--- locals.tf
+++ locals.tf
@@ -1,10 +1,16 @@
 locals {
-  profile        = "devops"
-  region         = "us-east-1"
+  profile                          = "devops"
+  region                           = "us-east-1"
   vpc_prefix                       = "dev"
   vpc_name                         = "main-vpc"
   vpc_cidr_block                   = "192.168.0.0/16"
   enable_dns_support               = true
   enable_dns_hostnames             = true
   assign_generated_ipv6_cidr_block = false
+  igw_name                         = "main_igw"
+  azs                              = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+  cidr_blocks                      = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24"]
+  public_ips                       = [true, false, true, false]
+  subnet_names                     = ["public-subnet-1a", "private-subnet-1b", "public-subnet-1c", "private-subnet-1d"]
+  route_table_name                 = "internet-route"
 }
--- modules/terraform-aws-vpc/main.tf
+++ modules/terraform-aws-vpc/main.tf
@@ -9,3 +9,28 @@ resource "aws_vpc" "main_vpc" {
     Name = "${var.vpc_prefix}-${var.vpc_name}"
   }
 }
+
+# Internet aws_internet_gateway
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.vpc_prefix}-${var.igw_name}"
+  }
+  depends_on = [aws_vpc.main_vpc]
+}
+
+# VPC Subnets
+resource "aws_subnet" "subnet" {
+  count                   = length(var.cidr_blocks)
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.cidr_blocks[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = var.public_ips[count.index]
+
+  tags = {
+    Name = "${var.vpc_prefix}-${var.subnet_names[count.index]}"
+  }
+  depends_on = [aws_vpc.main_vpc]
+}
